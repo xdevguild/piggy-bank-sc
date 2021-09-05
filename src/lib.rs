98c49c7fa8300ad32d496fd34e42296964e4b51c
Julian Ćwirko <julian.cwirko@gmail.com>
2021-09-06 00:41:43 +0200

    updates

--- src/lib.rs
+++ src/lib.rs
@@ -5,72 +5,103 @@ elrond_wasm::derive_imports!();
 
 #[elrond_wasm::contract]
 pub trait PiggyBank {
-	fn get_current_time(&self) -> u64 {
-		self.blockchain().get_block_timestamp()
-	}
+  fn get_current_time(&self) -> u64 {
+    self.blockchain().get_block_timestamp()
+  }
 
-	fn add(&self, val1: Self::BigUint, val2: Self::BigUint) -> Self::BigUint {
-		val1 + val2
-	}
+  fn add(&self, val1: Self::BigUint, val2: Self::BigUint) -> Self::BigUint {
+    val1 + val2
+  }
 
-	#[storage_set("owner")]
-    fn set_owner(&self, address: &Address);
-    #[storage_get("owner")]
-    fn get_owner(&self) -> Address;
+  #[init]
+  fn init(&self) -> SCResult<()> {
+    let my_address: Address = self.blockchain().get_caller();
+    self.set_owner(&my_address);
+    Ok(())
+  }
 
-	#[storage_set("amount")]
-	fn set_amount(&self, piggy_owner: &Address, sum: Self::BigUint);
+  #[endpoint]
+  fn amount(&self) -> SCResult<Self::BigUint> {
+    let caller = self.blockchain().get_caller();
+    
+    require!(
+      self.is_lock_time_empty(&caller) == false,
+      "You need to create your piggy bank first"
+    );
 
-	#[storage_get("amount")]
-	fn get_amount(&self, piggy_owner: &Address) -> Self::BigUint;
+    Ok(self.get_amount(&caller))
+  }
 
-	#[storage_set("lock_time")]
-    fn set_lock_time(&self, piggy_owner: &Address, lock_time: u64);
-    #[storage_get("lock_time")]
-    fn get_lock_time(&self, piggy_owner: &Address) -> u64;
-	#[storage_is_empty("lock_time")]
-	fn is_lock_time_empty(&self, piggy_owner: &Address) -> bool;
+  #[endpoint]
+  fn create_piggy(&self, lock_time: u64) -> SCResult<()> {
+    let caller = self.blockchain().get_caller();
+    require!(
+      self.is_lock_time_empty(&caller) == true,
+      "You already have one piggy"
+    );
+    require!(
+      lock_time > self.get_current_time(),
+      "Lock time should be in the future!"
+    );
+    self.set_lock_time(&caller, lock_time);
+    Ok(())
+  }
 
-	#[init]
-	fn init(&self) -> SCResult<()> {
-		let my_address: Address = self.blockchain().get_caller();
-		self.set_owner(&my_address);
-		Ok(())
-	}
+  #[endpoint]
+  #[payable("EGLD")]
+  fn add_amount(&self, #[payment] payment: Self::BigUint) -> SCResult<()> {
+    let caller = self.blockchain().get_caller();
+    require!(
+      self.is_lock_time_empty(&caller) == false,
+      "You need to create your piggy bank first"
+    );
+    let sum = self.get_amount(&caller);
+    let amount = self.add(sum, payment);
+    self.set_amount(&caller, amount);
+    Ok(())
+  }
 
-	#[endpoint]
-	fn amount(&self) -> SCResult<Self::BigUint> {
-		let caller = self.blockchain().get_caller();
-		require!(self.is_lock_time_empty(&caller) == false, "You need to create your piggy bank first");
-		Ok(self.get_amount(&caller))
-	}
+  #[endpoint]
+  fn pay_out(&self) -> SCResult<()> {
+    let caller = self.blockchain().get_caller();
+    require!(
+      self.get_lock_time(&caller) < self.get_current_time(),
+      "You can't withdraw your money yet"
+    );
+    require!(self.get_amount(&caller) > 0, "There is nothing to withdraw");
+    self.send()
+      .direct_egld(&caller, &self.get_amount(&caller), &[]);
 
-	#[endpoint]
-	fn create_piggy(&self, lock_time: u64) -> SCResult<()> {
-		let caller = self.blockchain().get_caller();
-		require!(self.is_lock_time_empty(&caller) == true, "You already have one piggy");
-		require!(lock_time > self.get_current_time(), "Lock time shouldn't be set for now");
-		self.set_lock_time(&caller, lock_time);
-		Ok(())
-	}
+    self.clear_amount(&caller);
+    self.clear_lock_time(&caller);
 
-    #[endpoint]
-	#[payable("EGLD")]
-	fn add_amount(&self, #[payment] payment: Self::BigUint) -> SCResult<()> {
-		let caller = self.blockchain().get_caller();
-		require!(self.is_lock_time_empty(&caller) == false, "You need to create your piggy bank first");
-		let sum = self.get_amount(&caller);
-		let amount = self.add(sum, payment);
-		self.set_amount(&caller, amount);
-		Ok(())
-	}
+    Ok(())
+  }
 
-	#[endpoint]
-	fn pay_out(&self) -> SCResult<()> {
-		let caller = self.blockchain().get_caller();
-		require!(self.get_lock_time(&caller) < self.get_current_time(), "You can't withdraw your money yet");
-		require!(self.get_amount(&caller) > 0, "There is nothing to withdraw");
-		self.send().direct_egld(&caller, &self.get_amount(&caller), &[]);
-		Ok(())
-	}
+  #[storage_set("owner")]
+  fn set_owner(&self, address: &Address);
+
+  #[storage_get("owner")]
+  fn get_owner(&self) -> Address;
+
+  #[storage_set("amount")]
+  fn set_amount(&self, piggy_owner: &Address, sum: Self::BigUint);
+
+  #[storage_get("amount")]
+  fn get_amount(&self, piggy_owner: &Address) -> Self::BigUint;
+
+  #[storage_clear("amount")]
+  fn clear_amount(&self, piggy_owner: &Address);
+
+  #[storage_set("lock_time")]
+  fn set_lock_time(&self, piggy_owner: &Address, lock_time: u64);
+
+  #[storage_get("lock_time")]
+  fn get_lock_time(&self, piggy_owner: &Address) -> u64;
+
+  #[storage_is_empty("lock_time")]
+  fn is_lock_time_empty(&self, piggy_owner: &Address) -> bool;
+
+  #[storage_clear("lock_time")]
+  fn clear_lock_time(&self, piggy_owner: &Address);
 }
